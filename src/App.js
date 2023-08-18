2ddbf40cb4dd95a338cc3586d7a630fed4646e85
Jasdeep singh <jasdeepsingh915119@icloud.com>
2023-08-18 22:28:13 +0530

    context folder created with files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Cart from "./Components/Cart";
+import Nav from "./Components/Nav";
 import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
-    <div className="App">
-      Ecommerce App
+    <div className='App'>
+      <Nav />
+      <Cart />
       <HomeScreen />
     </div>
   );
